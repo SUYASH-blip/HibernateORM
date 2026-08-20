ff5fed31146196086918af063966308a17fd4d75
suyash giri <suyashgiri938@gmail.com>
2026-08-20 20:10:08 +0530

    Transactional Methods Added

--- src/main/java/com/example/HibernateORM/repository/StudentRepository.java
+++ src/main/java/com/example/HibernateORM/repository/StudentRepository.java
@@ -1,7 +1,28 @@
 package com.example.HibernateORM.repository;
 
+import com.example.HibernateORM.model.Student;
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.PersistenceContext;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public class StudentRepository {
+    @PersistenceContext
+    private EntityManager entityManager;
+
+    //create
+    public void save(Student student){
+        entityManager.persist(student);
+    }
+
+    //read
+    public Student findById(Long id){
+        return entityManager.find(Student.class,id);
+    }
+
+    //delete
+    public void remove(Long id){
+        entityManager.remove(id);
+    }
+
 }
--- src/main/java/com/example/HibernateORM/service/StudentService.java
+++ src/main/java/com/example/HibernateORM/service/StudentService.java
@@ -2,26 +2,37 @@ package com.example.HibernateORM.service;
 
 import com.example.HibernateORM.model.Student;
 import com.example.HibernateORM.repository.StudentRepository;
+import jakarta.transaction.Transactional;
 import org.springframework.stereotype.Service;
 
 @Service
 public class StudentService {
-    private StudentRepository studentRepository;
+    private final StudentRepository studentRepository;
 
     public StudentService(StudentRepository studentRepository){
         this.studentRepository = studentRepository;
     }
 
+    @Transactional
     public void createStudent(Student studentreq){
-
+    studentRepository.save(studentreq);
     }
+
+    @Transactional
     public void getStudentById(Long id){
-
+        studentRepository.findById(id);
     }
+
+    @Transactional
     public void getAllStudents(){
-
     }
+
+    @Transactional
     public void updateStudent(Long id){
-        
+    }
+
+    @Transactional
+    public void deleteStudent(Long id){
+        studentRepository.remove(id);
     }
 }
